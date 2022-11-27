255fd218ae183db44949f7108aadd642ec916e76
Sindre Fonnes <sindre.fonnes@gmail.com>
2022-11-27 13:52:25 +0100

    trimming main file

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,17 @@ mod common_fn;
 mod data;
 mod handler;
 
-use data::{get_entries};
+use data::get_entries;
 use handler::handle;
 
 #[tokio::main]
 async fn main() {
     let entries = get_entries();
 
-    let stream = stream::iter(entries.iter().map(handle));
-
-    let result: Result<Vec<(String, f32)>, _> = stream.buffer_unordered(20).try_collect().await;
-
-    // let result = futures::future::try_join_all(futures).await;
+    let result: Result<Vec<(String, f32)>, _> = stream::iter(entries.iter().map(handle))
+        .buffer_unordered(30)
+        .try_collect()
+        .await;
 
     println!("{:#?}", result.unwrap());
 }
