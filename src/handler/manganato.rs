221751d3c6b772612a86dafa5426e7790dff6438
Sindre Fonnes <sindre.fonnes@gmail.com>
2024-11-20 16:01:02 +0100

    Updated the manganato parser to be better

--- src/handler/manganato.rs
+++ src/handler/manganato.rs
@@ -4,27 +4,31 @@ use super::CheckError;
 use regex::Regex;
 
 pub fn check(text: String, url: String) -> Result<f32, CheckError> {
-    let text: Vec<&str> = text
-        .lines()
-        .filter(|segment| segment.contains("chapter"))
-        .filter(|segment| segment.contains("</a>") && segment.len() < 2000)
-        .collect();
-
     let parse_error_message = format!("Error parsing manganato: {}", url);
 
-    parse_site_len_wrong(&text, parse_error_message.clone())?;
-
-    let text = text[0].split(">").skip(1).next().unwrap().to_lowercase();
-
-    let text: Vec<_> = text.split("chapter").collect();
+    let text = text.split("Chapter name").collect::<Vec<&str>>();
 
     parse_site_len_wrong(&text, parse_error_message.clone())?;
 
-    let get_number = Regex::new(r"([0-9])\w+").unwrap();
+    let text = text[1].split("</li>").collect::<Vec<&str>>();
+
+    parse_site_len_wrong(&text, parse_error_message.clone())?;
+
+    let text = text[0].split("<a").collect::<Vec<&str>>();
+
+    parse_site_len_wrong(&text, parse_error_message.clone())?;
+
+    let text = text[1].split(">").nth(1).unwrap().to_lowercase();
+
+    let text: Vec<&str> = text.split("chapter").collect();
+
+    parse_site_len_wrong(&text, parse_error_message.clone())?;
+
+    let get_number = Regex::new(r"(\d+(\.\d+)?)").unwrap();
 
     let chapter = get_number
         .captures(text[1])
-        .and_then(|captures| captures.get(0))
+        .and_then(|captures| captures.get(1))
         .map(|m| m.as_str())
         .ok_or(CheckError::Parse(
             "Couldn't find the chapter string".to_string(),
